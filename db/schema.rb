cfa982a6fc841c7ecb7cecd91af0a62497c4efc4
anwari-fikri <anwari.fikri@gmail.com>
2021-06-29 21:07:42 +0800

    Created Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_141132) do
+ActiveRecord::Schema.define(version: 2021_06_29_130237) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
