d0870e039cb9a3a5259c5bd41b422a0a63c23baf
anwari-fikri <anwari.fikri@gmail.com>
2021-06-30 23:15:08 +0800

    Allows creation of article in the frontend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,30 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+    @article = Article.new
   end
 
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render plain: @article.errors.full_messages
+    end
+  end
   
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 end
